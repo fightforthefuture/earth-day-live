20f72c2ec40e839d8efd0d297e2d2f9e34be7469
Dan Lewis-Toakley <d.lewistoakley@gmail.com>
2020-03-24 21:33:49 -0400

    added route for get-involved page

---
+++ pages/get-involved/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="container-fluid">
+      <div>
+        <logo />
+        <div class="col-md-6 offset-md-3">
+          <h1 class="title">
+            Strike, divest, and vote to save humanity and our planet.
+          </h1>
+        </div>
+      </div>
+    </div>
+    <PageFooter />
+  </div>
+</template>
+
+<script>
+  import Logo from '~/components/Logo.vue'
+  import PageFooter from '~/components/PageFooter.vue'
+
+  export default {
+    components: {
+      Logo,
+      PageFooter
+    }
+  }
+
+</script>
+
+<style>
+
+  body {
+    background-color: #e3e9ec;
+  }
+
+  .container-fluid {
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    background: url('~assets/images/earth-day-live-gradient.jpg') no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+    padding-bottom: 130px;
+  }
+
+  .title {
+    margin: 0 auto;
+    font-family: titling-gothic-fb, sans-serif;
+    display: block;
+    font-weight: 300;
+    color: #fff;
+    letter-spacing: 1px;
+    padding-bottom: 30px;
+  }
+
+</style>
