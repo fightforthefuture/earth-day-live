2a0b93b49c43ab2d102dad3c730001e59a890d80
Dan Lewis-Toakley <d.lewistoakley@gmail.com>
2020-03-25 23:57:04 -0400

    added styles for widget try it out button

--- components/WebsiteBanner.vue
+++ components/WebsiteBanner.vue
@@ -8,14 +8,34 @@
       <p class="description">
         Add our widget to your site. It's a small (but powerful!) notice to your site's users that something BIG is happening for Earth Day.
       </p>
+      <div class="row">
+        <div class="col-md-9 widget-code">
+          <code>
+            {{ widget_link }}
+          </code>
+        </div>
+        <div class="col-md-3 widget-code-button-wrapper">
+          <button>
+            Try it out
+            <img class="circle-arrow" src="~assets/images/circle-arrow.svg" alt="Arrow" />
+          </button>
+        </div>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
-    export default {
-        name: "WebsiteBanner"
+export default {
+  name: "WebsiteBanner",
+  data() {
+    return {
+      widget_link: "<script src=\"https://assets.earthdaylive2020.net/widget.js\" async><\/script>"
     }
+  }
+}
+
+
 </script>
 
 <style scoped>
@@ -31,5 +51,32 @@
 
   .separator {
     margin: auto;
+  }
+  .widget-code {
+    background-color: #fff;
+    text-align: left;
+    padding: 10px 20px;
+  }
+
+  .widget-code code {
+    color: #000;
+  }
+  .widget-code-button-wrapper {
+    padding-left: 0;
+  }
+
+  .widget-code-button-wrapper button {
+    width: 100%;
+    text-transform: uppercase;
+    color: #fff;
+    padding: 20px;
+    font-weight: 700;
+    background-image: linear-gradient(to right, #f26146 0%, #fb722c 35%, #f65b5a 65%, #f85371 100%) !important;
+  }
+
+  .widget-code-button-wrapper .circle-arrow {
+    width: 30px;
+    margin-top: -2px;
+    margin-left: 10px;
   }
 </style>
