bbb81e611096e0a131eb89702372e051240d24e6
megangoodling <megan.goodling@thoughtworks.com>
2020-04-01 14:11:42 -0400

    fixes to copy for final review

--- components/WhatsHappening.vue
+++ components/WhatsHappening.vue
@@ -8,9 +8,8 @@
         <p class="description">
           April 22 will be the 50th anniversary of Earth Day.
           That’s why we’re launching a massive livestream from April 22-24 where millions of people can tune in with activists, celebrities, musicians, and more in an epic celebration of our planet.
-          In the midst of an unprecented global pandemic, it's more important than ever to unite and support each other during this crisis.
-          Together, we’ll call on our leaders to take emergency action and drive donations to benefit the COVID-19 relief effort.
-          Here are all the ways you can spread the word and make this as big as possible.
+          In the midst of this global crisis, it's more important than ever to unite and support each other. Together, we'll support the relief effort and fight for a better world on the other side of the pandemic.
+          Here are all the ways you can help.
         </p>
       </div>
     </section>
