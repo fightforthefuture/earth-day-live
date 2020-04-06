004683e75e06145711c6e19b3ba90a5929e5903c
Dan Lewis-Toakley <d.lewistoakley@gmail.com>
2020-04-06 10:12:26 -0500

    added buttons for viewing widget instructions in common website platforms

--- components/WebsiteBanner.vue
+++ components/WebsiteBanner.vue
@@ -26,6 +26,19 @@
       </div>
       <div>
         <p class="description-center">
+          {{ translate.instructionsDescription }}
+        </p>
+        <div class="widget-instructions-list">
+          <b-list-group horizontal="lg">
+            <b-list-group-item class="flex-fill" @click="goToInstructions('https://docs.google.com/document/d/116ZqH8l68HaolxQQIxIuLwCuo-7EjnrCC_Cu76B3sEA/')">
+              {{ translate.squarespace }}
+            </b-list-group-item>
+            <b-list-group-item class="flex-fill" @click="goToInstructions('https://docs.google.com/document/d/1BvUz4f6hSrOcLhNSIBq4GUTeeA9Cvb6vdis3F0-Tnuo/')">
+              {{ translate.tumblr }}
+            </b-list-group-item>
+          </b-list-group>
+        </div>
+        <p class="description-center">
           {{ translate.center1 }} (<a href="https://widget.earthdaylive2020.org/demo.html" target="_blank">{{ translate.demo }}</a>)
         </p>
         <img class="widget-example" src="~assets/images/EDL-mockup-banner.png" alt="Website Banner Example 1" />
@@ -71,6 +84,9 @@ export default {
     },
     showWidget() {
       window.open('https://widget.earthdaylive2020.org/demo.html', '_blank')
+    },
+    goToInstructions(url) {
+      window.open(url, '_blank')
     }
   }
 }
@@ -131,9 +147,38 @@ export default {
     margin: 20px auto;
   }
 
+  .widget-instructions-list {
+    padding: 25px 0;
+  }
+
+  .list-group-item {
+    border-radius: 0 !important;
+    border: none;
+    border-right: 1px solid #fff;
+    background-color: #190825;
+    padding: 20px;
+    text-transform: uppercase;
+    font-family: titling-gothic-fb, sans-serif !important;
+    color: #fff;
+  }
+
+  .list-group-item:hover {
+    cursor: pointer;
+  }
+
+  .list-group-item:last-child {
+    border-right: none;
+  }
+
   @media only screen and (max-width: 600px) {
     .widget-code-button-wrapper {
       padding-right: 0;
+    }
+  }
+
+  @media only screen and (max-width: 992px) {
+    .list-group-item {
+      border-top: 1px solid #fff;
     }
   }
 </style>
