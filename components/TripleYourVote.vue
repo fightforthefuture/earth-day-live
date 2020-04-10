651bedb5f0fb7a4a5a3c25f91ff0a423f937c87d
Dan Lewis-Toakley <d.lewistoakley@gmail.com>
2020-04-10 12:55:56 -0500

    improved vote tripling section ui

--- components/TripleYourVote.vue
+++ components/TripleYourVote.vue
@@ -6,18 +6,18 @@
       </h1>
       <img class="separator" src="~assets/images/separator.jpg" :alt="$t('getInvolved.common.separatorAlt')" />
       <b-row>
-        <b-col cols="6">
+        <b-col cols="12" md="6" class="triple-your-vote-text">
           <p class="description">
             {{ translate.description1 }}
           </p>
           <h2 class="text-cta">
             {{ translate.textCta }}
           </h2>
-          <p class="description">
+          <p class="description text-cta-2">
             <strong>{{ translate.description2 }}</strong>
           </p>
         </b-col>
-        <b-col cols="6">
+        <b-col cols="12" md="6">
           <img class="triple-your-vote-image" src="~assets/images/EDL-Vote-Tripling.png" :alt="translate.tripleYourVoteImage" />
         </b-col>
       </b-row>
@@ -39,7 +39,10 @@
     text-align: center;
     background-color: #f8f8f8;
     padding: 40px 0;
-    display: none;
+
+  }
+  .triple-your-vote-image {
+    max-width: 100%;
   }
 
   .section-title {
@@ -51,5 +54,10 @@
     background: -webkit-linear-gradient(right, #f26146, #f47d3a, #f8d233, #cbdb2a, #88c656);
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
+    margin-top: 40px;
+    margin-bottom: 40px;
+  }
+  .text-cta-2 {
+    text-align: center;
   }
 </style>
