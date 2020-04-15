e8396f0ad1cb1197c693a342ae96fca7dc06ef36
AlexPerson <alex.person@thoughtworks.com>
2020-04-15 13:26:34 -0500

    [Alex/Charlie] Adds Maestro player to homepage

---
+++ app/components/MaestroLivestream.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container-fluid header-container">
+    <iframe id="maestro-player-iframe" src="https://sapphire.maestro.io/earth-day/?embed=theatre" style="width:100%">
+    </iframe>
+  </div>
+</template>
+
+<script>
+
+  function handleMessage(event) {
+    console.log(event)
+  }
+  export default {
+    name: "MaestroLivestream",
+    mounted() {
+      window.addEventListener('message', handleMessage)
+    }
+  }
+</script>
+
+<style scoped>
+  #maestro-player-iframe {
+    width: 100%;
+    border: none;
+    height: 1000px;
+  }
+</style>
