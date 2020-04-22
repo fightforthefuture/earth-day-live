9e3b9ecb7149659dd09e6242f82ee470460e1344
Dan Lewis-Toakley <d.lewistoakley@gmail.com>
2020-04-22 17:13:09 -0500

    showing button again on desktop

--- app/components/TakeActionButton.vue
+++ app/components/TakeActionButton.vue
@@ -1,17 +1,20 @@
 <style scoped>
-.take-action-button {
-  display: none;
-}
-
-@media only screen and (max-width: 899px) {
-  .take-action-button {
+  .take-action-button-wrapper {
+    background-color: #190825;
     display: block;
+    text-align: right;
+    padding-bottom: 10px;
+  }
+
+  .take-action-button {
+    display: inline-block;
     text-transform: uppercase;
     font-weight: bold;
     text-decoration: none;
     text-align: center;
     padding: 10px 30px;
     font-size: 20px;
+    width: 320px;
     color: #fff;
     background: linear-gradient(90deg, rgba(242, 97, 70, 1) 0%, rgba(251, 114, 44, 1) 35%, rgba(246, 91, 90, 1) 65%, rgba(248, 83, 113, 1)
     100%);
@@ -22,28 +25,40 @@
     margin-left: 10px;
     margin-top: -4px;
   }
-}
+
+  @media only screen and (max-width: 899px) {
+    .take-action-button-wrapper {
+      padding: 0;
+    }
+
+    .take-action-button {
+      width: 100%;
+      display: block;
+    }
+  }
 </style>
 
 <template>
-  <a class="take-action-button" href="#" @click.prevent="scrollTo('#signup')">
-    {{ $t('takeActionButton.buttonText') }}
-    <img src="~/assets/images/take-action-btn-arrow.svg" alt="">
-  </a>
+  <div class="take-action-button-wrapper">
+    <a class="take-action-button" @click.prevent="scrollTo('#signup')">
+      {{ $t('takeActionButton.buttonText') }}
+      <img src="~/assets/images/take-action-btn-arrow.svg" alt="">
+    </a>
+  </div>
 </template>
 
 <script>
-import { smoothScrollToElement } from '~/assets/js/helpers'
+  import { smoothScrollToElement } from '~/assets/js/helpers'
 
-export default {
-  methods: {
-    scrollTo(hash) {
-      const duration = 500
-      smoothScrollToElement(hash, duration)
-      setTimeout(() => {
-        location.hash = hash
-      }, duration)
+  export default {
+    methods: {
+      scrollTo(hash) {
+        const duration = 500
+        smoothScrollToElement(hash, duration)
+        setTimeout(() => {
+          location.hash = hash
+        }, duration)
+      }
     }
   }
-}
 </script>
