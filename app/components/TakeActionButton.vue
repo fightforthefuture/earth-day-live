6b8176e7eb561cc317731cc4976087741e2ededa
Ken Mickles <ken@37i.net>
2020-04-22 16:41:21 -0400

    Make TakeActionButton mobile-only + link to SignUpForm

--- app/components/TakeActionButton.vue
+++ app/components/TakeActionButton.vue
@@ -1,50 +1,35 @@
 <style scoped>
-.take-action-button-wrapper {
-  background-color: #190825;
-  display: block;
-  text-align: right;
-  padding-bottom: 10px;
-}
-
 .take-action-button {
-  display: inline-block;
-  text-transform: uppercase;
-  font-weight: bold;
-  text-decoration: none;
-  text-align: center;
-  padding: 10px 30px;
-  font-size: 20px;
-  width: 320px;
-  color: #fff;
-  background: linear-gradient(90deg, rgba(242, 97, 70, 1) 0%, rgba(251, 114, 44, 1) 35%, rgba(246, 91, 90, 1) 65%, rgba(248, 83, 113, 1)
-  100%);
-}
-
-.take-action-button img {
-  height: 20px;
-  margin-left: 10px;
-  margin-top: -4px;
+  display: none;
 }
 
 @media only screen and (max-width: 899px) {
-  .take-action-button-wrapper {
-    padding: 0;
+  .take-action-button {
+    display: block;
+    text-transform: uppercase;
+    font-weight: bold;
+    text-decoration: none;
+    text-align: center;
+    padding: 10px 30px;
+    font-size: 20px;
+    color: #fff;
+    background: linear-gradient(90deg, rgba(242, 97, 70, 1) 0%, rgba(251, 114, 44, 1) 35%, rgba(246, 91, 90, 1) 65%, rgba(248, 83, 113, 1)
+    100%);
   }
 
-  .take-action-button {
-    width: 100%;
-    display: block;
+  .take-action-button img {
+    height: 20px;
+    margin-left: 10px;
+    margin-top: -4px;
   }
 }
 </style>
 
 <template>
-  <div class="take-action-button-wrapper">
-    <a class="take-action-button" href="#register" @click.prevent="scrollTo('#register')">
-      {{ $t('takeActionButton.buttonText') }}
-      <img src="~/assets/images/take-action-btn-arrow.svg" alt="">
-    </a>
-  </div>
+  <a class="take-action-button" href="#" @click.prevent="scrollTo('#signup')">
+    {{ $t('takeActionButton.buttonText') }}
+    <img src="~/assets/images/take-action-btn-arrow.svg" alt="">
+  </a>
 </template>
 
 <script>
