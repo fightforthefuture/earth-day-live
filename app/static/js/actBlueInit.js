a4a1822a3d6e7356b78469518e0ca23c274fa0a9
Charlie <charlie.person@thoughtworks.com>
2020-04-23 14:08:18 -0400

    [Charlie/Alex] Adds thank you donation page and redirects upon contribution

--- app/static/js/actBlueInit.js
+++ app/static/js/actBlueInit.js
@@ -1,6 +1,8 @@
 (function(e){
-  console.log("ACT BLUE")
   e.actBlueConfig = {
     styleSheetHref: "https://stage.earthdaylive2020.org/donate/main.css"
   };
+  e.actBlueConfig = {onContribute: function(contribution) {
+	   e.location.href = "https://stage.earthdaylive2020.org/donation-thank-you/"
+  }};
 })(window);
